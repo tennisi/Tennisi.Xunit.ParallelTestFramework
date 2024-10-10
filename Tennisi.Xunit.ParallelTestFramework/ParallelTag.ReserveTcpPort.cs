ec2753d0f38692838b1a6a9fd5d16b2fe7909ed4
kgonsovsky@tennisi.it <kgonsovsky@tennisi.it>
2024-10-10 15:25:25 +0300

    doc & fix

---
+++ Tennisi.Xunit.ParallelTestFramework/ParallelTag.ReserveTcpPort.cs
@@ -0,0 +1,33 @@
+﻿namespace Tennisi.Xunit;
+
+public readonly partial struct ParallelTag
+{
+    internal const int MinTcpPort = 49152;
+    internal const int MaxTcpPort = 65535;
+    private static int _port = MinTcpPort-1;
+    private static readonly object PortLock = new();
+    
+    /// <summary>
+    /// Reserves a unique TCP port number within the predefined range (49152 - 65535).
+    /// </summary>
+    /// <returns>
+    /// A unique port number that corresponds to the reserved tag, constrained to the predefined range defined by the constants:
+    /// <list type="number">
+    /// <item><description>MinTestPort = 49152</description></item>
+    /// <item><description>MaxTestPort = 65535</description></item>
+    /// </list>
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when the maximum number of ports has been reserved, exceeding <see cref="MaxTcpPort"/>.
+    /// </exception>
+    public int ReserveTcpPort()
+    {
+        lock (PortLock)
+        {
+            _port++;
+            if (_port > MaxTcpPort)
+                throw new InvalidOperationException($"Maximum number of ports are captured: {MaxTcpPort}");
+            return _port;
+        }
+    }
+}
