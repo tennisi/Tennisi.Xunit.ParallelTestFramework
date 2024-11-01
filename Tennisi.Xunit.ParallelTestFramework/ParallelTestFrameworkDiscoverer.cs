9d4806fa0bcbbde2f6782918182b80d85e4a6f25
kgonsovsky@tennisi.it <kgonsovsky@tennisi.it>
2024-11-01 08:50:27 +0300

    retry

--- Tennisi.Xunit.ParallelTestFramework/ParallelTestFrameworkDiscoverer.cs
+++ Tennisi.Xunit.ParallelTestFramework/ParallelTestFrameworkDiscoverer.cs
@@ -1,4 +1,6 @@
-﻿using Xunit.Abstractions;
+﻿using System.Globalization;
+using Xunit;
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace Tennisi.Xunit;
@@ -6,16 +8,85 @@ namespace Tennisi.Xunit;
 public class ParallelTestFrameworkDiscoverer: XunitTestFrameworkDiscoverer
 {
     private readonly IAssemblyInfo _assemblyInfo;
+
     public ParallelTestFrameworkDiscoverer(IAssemblyInfo assemblyInfo, ISourceInformationProvider sourceProvider, IMessageSink diagnosticMessageSink, IXunitTestCollectionFactory collectionFactory = null) : base(assemblyInfo, sourceProvider, diagnosticMessageSink, collectionFactory)
     {
         _assemblyInfo = assemblyInfo;
     }
 
-    protected override bool FindTestsForMethod(ITestMethod testMethod, bool includeSourceInformation, IMessageBus messageBus,
+    protected override bool FindTestsForMethod(ITestMethod testMethod, bool includeSourceInformation,
+        IMessageBus messageBus,
         ITestFrameworkDiscoveryOptions discoveryOptions)
     {
-        ParallelSettings.RefineParallelSetting(_assemblyInfo.Name, discoveryOptions, "xunit.discovery.PreEnumerateTheories", true);
-        return base.FindTestsForMethod(testMethod, includeSourceInformation, messageBus, discoveryOptions);
+        ParallelSettings.RefineParallelSetting(_assemblyInfo.Name, discoveryOptions,
+            "xunit.discovery.PreEnumerateTheories", true);
+
+        var factAttributesRaw = testMethod.Method.GetCustomAttributes(typeof(FactAttribute));
+        var factAttributes = (factAttributesRaw as List<IAttributeInfo>) ?? factAttributesRaw.ToList();
+        if (factAttributes.Count > 1)
+        {
+            var message = string.Format(CultureInfo.CurrentCulture,
+                "Test method '{0}.{1}' has multiple [Fact]-derived attributes", testMethod.TestClass.Class.Name,
+                testMethod.Method.Name);
+            var testCase = new ExecutionErrorTestCase(DiagnosticMessageSink, TestMethodDisplay.ClassAndMethod,
+                TestMethodDisplayOptions.None, testMethod, message);
+            return ReportDiscoveredTestCase(testCase, includeSourceInformation, messageBus);
+        }
+
+        var factAttribute = factAttributes.FirstOrDefault();
+        if (factAttribute == null)
+            return true;
+
+        var factAttributeType = (factAttribute as IReflectionAttributeInfo)?.Attribute.GetType();
+
+        Type discovererType = null;
+        if (factAttributeType == null || !DiscovererTypeCache.TryGetValue(factAttributeType, out discovererType))
+        {
+            var testCaseDiscovererAttribute =
+                factAttribute.GetCustomAttributes(typeof(XunitTestCaseDiscovererAttribute)).FirstOrDefault();
+            if (testCaseDiscovererAttribute != null)
+            {
+                var args = testCaseDiscovererAttribute.GetConstructorArguments().Cast<string>().ToList();
+                discovererType = SerializationHelper.GetType(args[1], args[0]);
+            }
+
+            if (factAttributeType != null)
+                DiscovererTypeCache[factAttributeType] = discovererType;
+
+        }
+
+        if (discovererType == null)
+            return true;
+
+        var discoverer = GetDiscoverer(discovererType);
+        if (discoverer == null)
+            return true;
+
+        var useClassRetry = testMethod.ShouldUseClassRetry(out var classRetryCount);
+
+        foreach (var testCase in discoverer.Discover(discoveryOptions, testMethod, factAttribute))
+        {
+            if (useClassRetry)
+            {
+                var retryTestCase = new RetryTestCase(
+                    DiagnosticMessageSink,
+                    discoveryOptions.MethodDisplayOrDefault(),
+                    discoveryOptions.MethodDisplayOptionsOrDefault(),
+                    testMethod,
+                    classRetryCount,
+                    testCase.TestMethodArguments);
+                if (!ReportDiscoveredTestCase(retryTestCase, includeSourceInformation, messageBus))
+                    return false;
+            }
+            else
+            {
+                if (!ReportDiscoveredTestCase(testCase, includeSourceInformation, messageBus))
+                    return false;
+            }
+
+        }
+
+        return true;
     }
 
     protected override bool FindTestsForType(ITestClass testClass, bool includeSourceInformation, IMessageBus messageBus,
---
+++ Tennisi.Xunit.ParallelTestFramework/RetryTestCase.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Tennisi.Xunit;
+
+[Serializable]
+internal class RetryTestCase : XunitTestCase
+{
+    private int _retryCount;
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("Called by the de-serializer; should only be called by deriving classes for de-serialization purposes")]
+    public RetryTestCase()
+    {
+    }
+
+    public RetryTestCase(IMessageSink diagnosticMessageSink, TestMethodDisplay testMethodDisplay,
+        TestMethodDisplayOptions defaultMethodDisplayOptions, ITestMethod testMethod, int retryCount,
+        object[] testMethodArguments)
+        : base(diagnosticMessageSink, testMethodDisplay, defaultMethodDisplayOptions, testMethod,
+            testMethodArguments: testMethodArguments)
+    {
+        _retryCount = retryCount;
+    }
+    
+    public override async Task<RunSummary> RunAsync(IMessageSink diagnosticMessageSink,
+        IMessageBus messageBus,
+        object[] constructorArguments,
+        ExceptionAggregator aggregator,
+        CancellationTokenSource cancellationTokenSource)
+    {
+        var runCount = 0;
+
+        while (true)
+        {
+            var delayedMessageBus = new DelayedMessageBus(messageBus);
+
+            var summary = await base.RunAsync(diagnosticMessageSink, delayedMessageBus, constructorArguments,
+                aggregator, cancellationTokenSource);
+            if (aggregator.HasExceptions || summary.Failed == 0 || ++runCount >= _retryCount)
+            {
+                delayedMessageBus.Dispose();
+                return summary;
+            }
+
+            diagnosticMessageSink.OnMessage(
+                new DiagnosticMessage("Execution of '{0}' failed (attempt #{1}), test retrying...", DisplayName, runCount));
+        }
+    }
+
+    public override void Serialize(IXunitSerializationInfo data)
+    {
+        base.Serialize(data);
+        data.AddRetryCount(_retryCount);
+    }
+
+    public override void Deserialize(IXunitSerializationInfo data)
+    {
+        base.Deserialize(data);
+        _retryCount = data.GetRetryCount();
+    }
+}
