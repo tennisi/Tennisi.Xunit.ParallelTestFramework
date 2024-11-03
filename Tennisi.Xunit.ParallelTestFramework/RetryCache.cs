a0b56b08eccc09c4c60837bca39f8b5a609d12fb
kgonsovsky@tennisi.it <kgonsovsky@tennisi.it>
2024-11-03 10:00:53 +0300

    retry fix 2

--- Tennisi.Xunit.ParallelTestFramework/RetryCache.cs
+++ Tennisi.Xunit.ParallelTestFramework/RetryCache.cs
@@ -5,36 +5,46 @@ namespace Tennisi.Xunit;
 
 internal static class RetryCache
 {
-    private static readonly ConcurrentDictionary<ITestMethod, bool> MethodCache = new();
-    
-    private static readonly ConcurrentDictionary<ITypeInfo, int> TypeCache = new();
-    
+    private static readonly ConcurrentDictionary<(ITestMethod? Method, ITypeInfo? Class), RetryEntry> Cache = new();
+
     internal static bool ShouldUseClassRetry(this ITestMethod testMethod, out int retryCount)
     {
-        var result = MethodCache.GetOrAdd(testMethod, mtd =>
+        var methodEntry = Cache.GetOrAdd((testMethod, null), tuple =>
         {
-            var isMethodAttribute = mtd.Method.GetCustomAttributes(typeof(RetryFactAttribute)).Any()
-                                    || mtd.Method.GetCustomAttributes(typeof(RetryTheoryAttribute)).Any();
-
-            return isMethodAttribute;
+            var mtd = tuple.Method;
+            var hasMethodAttribute = mtd?.Method.GetCustomAttributes(typeof(RetryFactAttribute)).Any() == true
+                                     || mtd?.Method.GetCustomAttributes(typeof(RetryTheoryAttribute)).Any() == true;
+            return new RetryEntry(hasMethodAttribute, int.MinValue);
         });
 
-        if (!result)
+        if (methodEntry.HasRetry)
         {
-            var cnt = TypeCache.GetOrAdd(testMethod.TestClass.Class, cls =>
-            {
-                var retryClassAttribute = cls
-                    .GetCustomAttributes(typeof(RetryClassAttribute))
-                    .FirstOrDefault();
-                return retryClassAttribute.GetRetryCountOrDefault();
-            });
-            if (!cnt.IsDefaultRetryCount())
-            {
-                retryCount = cnt;
-                return true;
-            }
+            retryCount = int.MinValue;
+            return false;
         }
-        retryCount = int.MinValue;
-        return false;
+        
+        var classEntry = Cache.GetOrAdd((null, testMethod.TestClass.Class), tuple =>
+        {
+            var cls = tuple.Class;
+            var retryClassAttribute = cls?.GetCustomAttributes(typeof(RetryClassAttribute)).FirstOrDefault();
+            var retryCount = retryClassAttribute.GetRetryCountOrDefault();
+            var hasRetry = retryClassAttribute != null && !retryCount.IsDefaultRetryCount();
+            return new RetryEntry(hasRetry, retryCount);
+        });
+        
+        retryCount = classEntry.RetryCount;
+        return classEntry.HasRetry;
+    }
+
+    private readonly struct RetryEntry
+    {
+        public bool HasRetry { get; }
+        public int RetryCount { get; }
+
+        public RetryEntry(bool hasRetry, int retryCount)
+        {
+            HasRetry = hasRetry;
+            RetryCount = retryCount;
+        }
     }
 }
