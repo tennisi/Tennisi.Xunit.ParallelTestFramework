2072e05b6addde21222453e2a2a62b70374414d2
kgonsovsky@tennisi.it <kgonsovsky@tennisi.it>
2024-12-12 18:35:56 +0300

    206

---
+++ Tennisi.Xunit.ParallelTestFramework.UI.Tests/TestWindowTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Tennisi.Xunit.ParallelTestFramework.UI.Tests.Forms;
+
+
+namespace Tennisi.Xunit.ParallelTestFramework.UI.Tests;
+
+public class TestWindowTests
+{
+    [StaFact]
+    public void ItShouldClick()
+    {
+
+        var window = new TestWindow();
+
+        // Act
+        var button = window.PublicButton;
+
+        // Assert
+        Assert.NotNull(button);
+        Assert.Equal("Click Me", button.Content);
+    }
+}
